cdd2dc1b5bd9b85d341c27664bdfcbacdc5c0703
Zac Edens <themancalledzac@gmail.com>
2021-03-05 15:32:37 -0800

    updated routes stuff. will change based off our actual DB needs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
+const path = require("path");
 const router = require("express").Router();
 const userRoutes = require("./user");
 
 // put a post route here?
+router.use("/api", userRoutes);
+
+// if no API routes are hit, send the React app
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
