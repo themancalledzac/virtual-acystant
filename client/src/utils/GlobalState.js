a2dfb95ff129e7ba8b52b6f31b5599a120c4f0fa
Zac Edens <themancalledzac@gmail.com>
2021-03-05 16:13:22 -0800

    GlobalState with lots of comments. rough switch-case, rough state that matches the user Model for the db.

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -9,6 +9,8 @@ const { Provider } = UserContext;
 
 // reducer portion of the useReducer
 // our current state, and the action called by our 'dispatch' in the useReducer hook
+// we call this reducer to update our state on the page. What we need to remember is that the state also will need to update on the backend, as it's the same data. so every 'update' will need to be a 'dispatch' update as well as a dbUpdate in the routes folder.
+// all our 'dispatch' calls will be on the page or component it's called from. We will most likely do that for both calls?
 const reducer = (state, action) => {
   switch (action.type) {
     case CREATE_USER:
@@ -39,7 +41,9 @@ const UserProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useReducer(
     (reducer,
     {
-      // here lies our state
+      // here lies our state.
+      // do we store images our our state? Need to ask TA/Anthony
+      // realistically, we are only using state once a user logs in, so we can pass this data around the site, for directions to a doctor
       user: {
         _id: 0,
         firstName: "",
