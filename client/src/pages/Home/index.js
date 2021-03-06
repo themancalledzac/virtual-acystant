be436650d4198af9c15fbc5493a173794c26aef9
Zac Edens <themancalledzac@gmail.com>
2021-03-06 10:01:50 -0800

    more notes

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+const addUser = () => {
+  // send data to backend
+  // util/API.js file
+  // dispatch({
+  //   type: ADD_USER,
+  //   payload: newUser
+  // })
+};
+
 const Home = () => {
   return (
     <div>
       <h1>HomePage</h1>
+      <form onSubmit={addUser}>
+        <input></input>
+      </form>
     </div>
   );
 };
