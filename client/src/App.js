257cde01445028695ad3fcf945e44047813ca334
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-03-13 08:13:44 -0800

    Updated the app and index with Store Provider and reauth for the react app

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/Main";
-import { UserProvider } from "./utils/GlobalState";
+import { UserProvider } from "./store/index";
+// Import the useAuthTokenStore hook.
+import { useAuthTokenStore } from "./utils/auth";
 
 function App() {
+
+  // Use the hook to reauthenticate stored tokens.
+  useAuthTokenStore();
+
   return (
     <Router>
       <UserProvider>
