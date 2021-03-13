848a37053c2aeb3c17b2f1502b77478f023136ba
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-03-13 08:02:11 -0800

    Added validation and authentication routes

--- models/user.js
+++ models/user.js
@@ -2,16 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstName: {
-    type: String, 
-    required: true },
-  lastName: { 
-    type: String, 
-    required: true },
-  email: { 
-    type: String, 
-    required: true, 
-    unique: true },
+  email: {
+    type: String,
+    required: true
+  },
   password: {
     type: String,
     required: true
