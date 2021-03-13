7fe79a23b71efb19766e44ffe174e8b731ae725f
Max Lechner <mkonlech@gmail.com>
2021-03-13 11:40:02 -0800

    save index file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
-<<<<<<< HEAD
 const FileUpload = require("./FileUpload");
-=======
 // const imageProcessing = require("./imageProcessing");
->>>>>>> main
 
 router.use("/users", userRoutes);
 router.use("/FileUpload", FileUpload);
