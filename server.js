56673c7e24fbc2fc9b5cda73f9159d9b742a03c7
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-03-13 08:07:49 -0800

    Configured server.js to include .env, passport, and api routes configuration

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+require("dotenv").config();
+
+const passport = require("passport");
+
+app.use(passport.initialize());
+// Passport config
+passport.use( require("./config/jwtPassportStrategy") );
+
 const express = require("express");
 const routes = require("./routes/api")
 
@@ -15,9 +23,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-// app.use("/api", routes);
-
-
+app.use( "/api", require("./routes/authentication") );
 
 // Connect to the Mongo DB
 mongoose.connect(
