a8e41538c6d0a747240f6ece47fb613a92683b8b
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-03-13 08:29:10 -0800

    Created a new SignupForm component

---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,44 @@
+function SignupForm() {
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    // Get the helper login function from the `useLogin` hook.
+    const login = useLogin();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        try {
+
+            // Register the user.
+            await api.register({ email, password });
+
+            // User has been successfully registered, now log them in with the same information.
+            await login({ email, password });
+
+            // User has been successfully registered, logged in and added to state. Perform any additional actions you need here such as redirecting to a new page.
+
+        } catch(err) {
+
+             // Handle error responses from the API. This will include
+             if( err.response && err.response.data ) console.log(err.response.data);
+             
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Register</h2>
+            <input type="text" ref={emailRef} placeholder="Your email" /><br />
+            <input type="password" ref={passwordRef} placeholder="Your password" /><br />
+            <button>Submit</button>
+        </form>
+    )
+
+}
+
+export default SignupForm;
