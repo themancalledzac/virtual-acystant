9312fd9a92ba84c793ddab22ff1a752d5e7dbf1b
Zac Edens <themancalledzac@gmail.com>
2021-03-16 11:41:49 -0700

    added notes for future 'uploadData function for returning axios post

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,64 +1,52 @@
 import axios from "axios";
 
+// export default {
+//   uploadData: function (top3, result) {
+//     return axios.post("/api/data", top3, result);
+//   },
+// };
+
 class API {
+  axios;
 
-    axios;
+  constructor() {
+    this.axios = axios.create();
+  }
 
-    constructor() {
+  /**
+   * @param {String} name
+   * @param {String} value
+   */
+  setHeader(name, value) {
+    if (value) this.axios.defaults.headers.common[name] = value;
+    else delete this.axios.defaults.headers.common[name];
+  }
 
-        this.axios = axios.create();
+  /**
+   * @param {object} userData
+   * @param {String} userData.email
+   * @param {String} userData.password
+   *
+   * @returns {Promise}
+   */
+  register(userData) {
+    return this.axios.post("/api/register", userData);
+  }
 
-    }
+  /**
+   * @param {object} userData
+   * @param {String} userData.email
+   * @param {String} userData.password
+   *
+   * @returns {Promise}
+   */
+  login(userData) {
+    return this.axios.post("/api/login", userData);
+  }
 
-    /**
-     * @param {String} name 
-     * @param {String} value 
-     */
-    setHeader( name, value ) {
-
-        if( value )
-
-            this.axios.defaults.headers.common[name] = value;
-
-        else
-
-            delete this.axios.defaults.headers.common[name];
-
-    }
-
-    /**
-     * @param {object} userData 
-     * @param {String} userData.email
-     * @param {String} userData.password
-     * 
-     * @returns {Promise}
-     */
-    register( userData ) {
-
-        return this.axios.post("/api/register", userData);
-
-    }
-
-
-    /**
-     * @param {object} userData 
-     * @param {String} userData.email
-     * @param {String} userData.password
-     * 
-     * @returns {Promise}
-     */
-    login( userData ) {
-
-        return this.axios.post("/api/login", userData);
-
-    }
-
-    authenticated() {
-
-        return this.axios.post("/api/authenticated");
-
-    }
-
+  authenticated() {
+    return this.axios.post("/api/authenticated");
+  }
 }
 
 export default new API();
