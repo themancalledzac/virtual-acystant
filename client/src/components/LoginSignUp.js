9d886751137d254c7fe862cf2f525c374dbd52ea
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-03-17 16:41:57 -0700

    Added travis.yml

--- client/src/components/LoginSignUp.js
+++ client/src/components/LoginSignUp.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Login from "../components/Login";
-import SignUp from "../components/SignUp";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 // -----------------------------------------------------------------------------//
 //                                                                              //
