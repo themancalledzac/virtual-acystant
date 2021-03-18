71d5da07c65b92680544392e5e26ee661eae9d63
Zac Edens <themancalledzac@gmail.com>
2021-03-17 22:00:28 -0700

    merge changes

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
+// import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
