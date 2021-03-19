1fbd4864709b8a7c9dffe15a161502376afc3da7
Zac Edens <themancalledzac@gmail.com>
2021-03-19 15:17:11 -0700

    updated filupload page for proper card, first attempt at saving state from findings data

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,8 +1,40 @@
 import React from "react";
 import * as tf from "@tensorflow/tfjs";
-// import * from "../../../tfjs-models/model.json"
+import {
+  Container,
+  Grid,
+  makeStyles,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import colors from "./colors";
+import { useStoreContext } from "../store";
+import { RETURN_DATA } from "../store/action";
 
+// -------------------------------- PAGE STYLING----------------------------------------//
+const useStyles = makeStyles((theme) => ({
+  searchBar: {
+    marginTop: theme.spacing(15),
+    paddingTop: theme.spacing(5),
+    paddingBottom: theme.spacing(5),
+    backgroundColor: colors.primary,
+    marginBottom: theme.spacing(15),
+  },
+  title: {
+    padding: "10px",
+  },
+  image: {
+    maxWidth: "400px",
+    maxHeight: "auto",
+  },
+  uploader: {
+    marginBottom: "20px",
+  },
+}));
+
+// import * from "../../../tfjs-models/model.json"
 export default function FileUpload() {
+  const [, dispatch] = useStoreContext();
   const CLASSES = {
     0: "Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowenâ€™s disease)",
     1: "Basal Cell Carcinoma",
@@ -19,7 +51,7 @@ export default function FileUpload() {
 
   const [result, setResult] = React.useState("");
   const [findings, setFindings] = React.useState("");
-  console.log("These are findings" + findings)
+  console.log("These are findings" + findings);
 
   function uploader(e) {
     const imageFile = e.target.files[0];
@@ -35,7 +67,7 @@ export default function FileUpload() {
     reader.readAsDataURL(imageFile);
     // console.log(result);
     console.log("Image file " + imageFile);
-    return imageFile
+    return imageFile;
   }
 
   async function predict() {
@@ -61,22 +93,22 @@ export default function FileUpload() {
         return b.probability - a.probability;
       })
       .slice(0, 3);
-    // db save
-    // loadData(top3, result);
-    // db(top5)
-    // state save
+    // ???
+    dispatch({ type: RETURN_DATA, payload: { top3 } });
+
     console.log(top3);
     // return top3[0].probability + ", " +top3[1].probability + ", " + top3[2].probability
-    return top3
+    // -----------------------------GLOBALSTATE SAVE -------------------------------------//
+    return top3;
   }
 
-  const showResults = async () => setFindings( await predict() );
+  const showResults = async () => setFindings(await predict());
 
-
+  // dispatch({ type: RETURN_DATA, payload: findings });
 
   const saveResults = async () => {
     // API.saveResults(findings, result)
-  }
+  };
 
   // db function that takes in predict data and then
   // calls state function to save data
@@ -90,29 +122,91 @@ export default function FileUpload() {
   // call to update local state
   // function stateChange() {};
 
+  const classes = useStyles();
+
   return (
     <>
-      <div className='image'>
-        <h2> Upload skin image</h2>
-        <input
-          type='file'
-          ref={imageRef}
-          onChange={(e) => {
-            setImage(e.target.files[0]);
-            uploader(e);
-            console.log("from onClick"+e.target.files[0]);
-            console.log("JSON stuff" + image[0]);
-          }}
-        />
-        {result && <img ref={imageRef} src={result} alt='' />}
-        {result && <button onClick={predict}> Predict</button>}
-        {result && <button onClick={showResults}> Show Results</button>}
-        {findings && <button onClick={saveResults}> Save Results</button>}
-        {/* {findings && <p> Findings are: {findings}</p>} */}
-        {findings && <h1> Findings are: </h1>}
-        {findings && findings.map((item, index) => (
-        <li key={index}> {item.className} with probability {item.probability}</li>))}
-      </div>
+      <Container className={classes.searchBar} maxWidth='md'>
+        <Grid container spacing={2}>
+          <Grid item xs>
+            <h2 className={classes.title}>Upload Skin Image</h2>
+
+            <input
+              className={classes.uploader}
+              type='file'
+              inputRef={imageRef}
+              onChange={(e) => {
+                setImage(e.target.files[0]);
+                uploader(e);
+              }}
+              variant='outlined'
+              margin='normal'
+              required
+              fullWidth
+              label='Image Upload'
+              autoFocus
+            />
+            {result && (
+              <img
+                ref={imageRef}
+                src={result}
+                alt=''
+                className={classes.image}
+              />
+            )}
+            {result && <button onClick={predict}> Predict</button>}
+            {result && <button onClick={showResults}> Show Results</button>}
+            {findings && <button onClick={saveResults}> Save Results</button>}
+            {/* {findings && <p> Findings are: {findings}</p>} */}
+            {findings && <h1> Findings are: </h1>}
+            {findings &&
+              findings.map((item, index) => (
+                <li key={index}>
+                  {" "}
+                  {item.className} with a probability of{" "}
+                  {item.probability.toFixed(3) * 100 + "%"}
+                </li>
+              ))}
+          </Grid>
+          <Grid item xs>
+            <h2 className={classes.title}>Results</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+              veniam quis error. Accusamus similique animi fugit necessitatibus
+              eum repellat asperiores dignissimos, temporibus reprehenderit
+              dolorum ab aliquam. Qui quas ex fuga.
+              {/* {diseaseLookupResults} */}
+            </p>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
+
+// <div className='image'>
+//   <h2> Upload skin image</h2>
+//   <input
+//     type='file'
+//     ref={imageRef}
+//     onChange={(e) => {
+//       setImage(e.target.files[0]);
+//       uploader(e);
+//       console.log("from onClick" + e.target.files[0]);
+//       console.log("JSON stuff" + image[0]);
+//     }}
+//   />
+//   {result && <img ref={imageRef} src={result} alt='' />}
+//   {result && <button onClick={predict}> Predict</button>}
+//   {result && <button onClick={showResults}> Show Results</button>}
+//   {findings && <button onClick={saveResults}> Save Results</button>}
+//   {/* {findings && <p> Findings are: {findings}</p>} */}
+//   {findings && <h1> Findings are: </h1>}
+//   {findings &&
+//     findings.map((item, index) => (
+//       <li key={index}>
+//         {" "}
+//         {item.className} with probability {item.probability}
+//       </li>
+//     ))}
+// </div>;
