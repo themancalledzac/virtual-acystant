41c68ed31e3acc280dbddb1717b8477d36e1dcb6
Kat Gomozova <kathy.gomozova@gmail.com>
2021-03-24 21:40:15 -0700

    loading results in a separate card on toggle

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -264,7 +264,7 @@ export default function FileUpload() {
           }
         </Grid>
         <Grid>
-          <LoadResults />
+          {/* <LoadResults /> */}
         </Grid>
       </Container>
     </>
