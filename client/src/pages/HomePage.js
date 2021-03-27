79d723976e80c3a5495c6d57076c6fde5f88406d
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-03-27 06:08:59 -0700

    Added new favicon

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,14 @@
 import HeaderCard from "../components/HeaderCard";
 import headerCardData from "../utils/headerCardData.json";
-
+import React from "react";
+import { Container, Button } from "@material-ui/core";
 import FileUpload from "../components/FileUpload";
 // import Wikipedia from "../components/Wikipedia";
-import { useStoreContext } from "../store";
 import PreviousResults from "../components/PreviousResults";
-// import doctorImage from "../assets/images/pexels-anthony-shkraba-5214995.jpg";
-import backgroundImage from "../assets/images/pexels-karolina-grabowska-4021775.jpg";
+import doctorImage from "../assets/VAbluewhitelogo.png";
 import Wikipedia from "../components/Wikipedia";
+import LoadResults from "../components/LoadResults";
+import { useStoreContext } from "../store/index";
 
 const addUser = () => {
   // send data to backend
@@ -21,15 +22,27 @@ const addUser = () => {
 const Home = () => {
   const [state] = useStoreContext();
   console.log(state);
+  const [show, toggleShow] = React.useState(true);
+
   return (
     <div>
       <HeaderCard
         title={headerCardData.home.title}
         paragraph={headerCardData.home.paragraph}
-        image={backgroundImage}
+        image={doctorImage}
+        disclaimer={headerCardData.home.disclaimer}
       />
       <FileUpload />
-      
+      <Container maxWidth='lg'>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => toggleShow(!show)}
+        >
+          saved results: {show ? "hide" : "show"}
+        </Button>
+        {show && <LoadResults />}
+      </Container>
       {/* <PreviousResults /> */}
       {/* <form onSubmit={addUser}></form> */}
     </div>
