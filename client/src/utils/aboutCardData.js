7b4cfcae5d52bd0abb67919872428588d9de941f
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-03-27 06:52:19 -0700

    Updated aboutCardData

--- client/src/utils/aboutCardData.js
+++ client/src/utils/aboutCardData.js
@@ -3,28 +3,24 @@ const AboutCardData = [
       "name": "Kat Gomozova",
       "github": "https://github.com/kgo87",
       "linkedin": "linkedin",
-      "porfolio":"portfolio",
       "image": "../assets/kat-gomozova.png"
     },
     {
         "name": "Macz Norton",
         "github": "https://github.com/macz-norton",
         "linkedin": "linkedin",
-        "porfolio":"portfolio",
         "image": "../assets/macz-norton.png"
     },
     {
         "name": "Max Lechner",
         "github": "https://github.com/maxlechner",
         "linkedin": "linkedin",
-        "porfolio":"portfolio",
         "image": "../assets/max-lechner.png"
     },
     {
         "name": "Zechariah Edens ",
         "github": "https://github.com/themancalledzac",
         "linkedin": "linkedin",
-        "porfolio":"portfolio",
         "image": "../assets/zac-edens.png"
       }
 ]
